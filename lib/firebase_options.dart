260d899cd9f66aee61d679f8c07274bed796c80e
bondhu-app <md.mojidul.haque.1234@gmail.com>
2026-08-26 20:44:21 +0600

    Modify FirebaseOptions to use Android settings
    Updated the FirebaseOptions class to return the Android options for all platforms instead of specific options for web, iOS, macOS, windows, and linux.

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,75 +1,29 @@
-// File: lib/firebase_options.dart
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
-      return web;
+      return android;
     }
 
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
         return android;
-
       case TargetPlatform.iOS:
-        return ios;
-
+        return android;
       case TargetPlatform.macOS:
-        return macos;
-
+        return android;
       case TargetPlatform.windows:
-        return windows;
-
+        return android;
       case TargetPlatform.linux:
-        return linux;
+        return android;
+      case TargetPlatform.fuchsia:
+        return android;
     }
   }
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyAbYCTMxhCJzft1dgnpHDRkbC4v9RMU4GE',
-    appId: '1:1074705827775:android:68710467504787bc3a430a',
-    messagingSenderId: '1074705827775',
-    projectId: 'friend-post-fbfdd',
-    storageBucket: 'friend-post-fbfdd.firebasestorage.app',
-  );
-
-  static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyAbYCTMxhCJzft1dgnpHDRkbC4v9RMU4GE',
-    appId: '1:1074705827775:android:68710467504787bc3a430a',
-    messagingSenderId: '1074705827775',
-    projectId: 'friend-post-fbfdd',
-    storageBucket: 'friend-post-fbfdd.firebasestorage.app',
-  );
-
-  static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyAbYCTMxhCJzft1dgnpHDRkbC4v9RMU4GE',
-    appId: '1:1074705827775:android:68710467504787bc3a430a',
-    messagingSenderId: '1074705827775',
-    projectId: 'friend-post-fbfdd',
-    storageBucket: 'friend-post-fbfdd.firebasestorage.app',
-    iosBundleId: 'com.friendpost.app',
-  );
-
-  static const FirebaseOptions macos = FirebaseOptions(
-    apiKey: 'AIzaSyAbYCTmMxhCJzft1dgnpHDRkbC4v9RMU4GE',
-    appId: '1:1074705827775:android:68710467504787bc3a430a',
-    messagingSenderId: '1074705827775',
-    projectId: 'friend-post-fbfdd',
-    storageBucket: 'friend-post-fbfdd.firebasestorage.app',
-    iosBundleId: 'com.friendpost.app',
-  );
-
-  static const FirebaseOptions windows = FirebaseOptions(
-    apiKey: 'AIzaSyAbYCTmMxhCJzft1dgnpHDRkbC4v9RMU4GE',
-    appId: '1:1074705827775:android:68710467504787bc3a430a',
-    messagingSenderId: '1074705827775',
-    projectId: 'friend-post-fbfdd',
-    storageBucket: 'friend-post-fbfdd.firebasestorage.app',
-  );
-
-  static const FirebaseOptions linux = FirebaseOptions(
     apiKey: 'AIzaSyAbYCTmMxhCJzft1dgnpHDRkbC4v9RMU4GE',
     appId: '1:1074705827775:android:68710467504787bc3a430a',
     messagingSenderId: '1074705827775',
